2af7fb31f27534d6f6f0acbbb3d62bc51b723bf2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-06-13 06:40:15 +0100

    Width And Height of RectangularGeometry Updated to Cover Irregular Shapes

--- SAM_gbXML/SAM.Geometry.gbXML/Convert/TogbXML/RectangularGeometry.cs
+++ SAM_gbXML/SAM.Geometry.gbXML/Convert/TogbXML/RectangularGeometry.cs
@@ -12,11 +12,22 @@ namespace SAM.Geometry.gbXML
 
             Plane plane = closedPlanar3D.GetPlane();
             Planar.BoundingBox2D boundingBox2D = plane.Convert(closedPlanar3D).GetBoundingBox();
+            double area_closedPlanar3D = closedPlanar3D.GetArea();
+            double area_boundingBox2D = boundingBox2D.GetArea();
+
+            double width = boundingBox2D.Width;
+            double height = boundingBox2D.Height;
+            if(System.Math.Abs(area_closedPlanar3D - area_boundingBox2D) > Core.Tolerance.MacroDistance)
+            {
+                double factor = area_closedPlanar3D / area_boundingBox2D;
+                width = factor * width;
+                height = factor * height;
+            }
 
             RectangularGeometry rectangularGeometry = new RectangularGeometry();
             rectangularGeometry.Azimuth = Query.Azimuth(closedPlanar3D, Vector3D.WorldY).ToString();
-            rectangularGeometry.Width = boundingBox2D.Width.ToString();
-            rectangularGeometry.Height = boundingBox2D.Height.ToString();
+            rectangularGeometry.Width = width.ToString();
+            rectangularGeometry.Height = height.ToString();
             rectangularGeometry.CartesianPoint = plane.Convert(boundingBox2D.Min).TogbXML(tolerance);
             rectangularGeometry.Tilt = Query.Tilt(closedPlanar3D).ToString();
             return rectangularGeometry;
