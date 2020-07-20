39c8108b92ee59040b0bf38782315ecc50ff009c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-07-20 21:56:26 +0100

    Rename Space Added

---
+++ SAM_gbXML/SAM.Analytical.gbXML/Modify/Rename.cs
@@ -0,0 +1,16 @@
+﻿using gbXMLSerializer;
+
+namespace SAM.Analytical.gbXML
+{
+    public static partial class Modify
+    {
+        public static bool Rename(this gbXMLSerializer.Space space, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            space.Name = name;
+            return true;
+        }
+    }
+}
---
+++ SAM_gbXML/SAM.Analytical.gbXML/Query/Spaces.cs
@@ -0,0 +1,14 @@
+﻿using gbXMLSerializer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.gbXML
+{
+    public static partial class Query
+    {
+        public static List<gbXMLSerializer.Space> Spaces(this Building building)
+        {
+            return building?.Spaces?.ToList();
+        }
+    }
+}
