bfbc3ac3c7103e053a1aa8a486557a6b83da0fd9
michaldengusiak <tassimulation@gmail.com>
2023-01-20 13:12:47 +0000

    fixes

---
+++ SAM_gbXML/SAM.Analytical.gbXML/Convert/ToFile/ToFile.cs
@@ -0,0 +1,26 @@
+﻿using gbXMLSerializer;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.gbXML
+{
+    public static partial class Convert
+    {
+        public static bool ToFile(this AnalyticalModel analyticalModel, string path, double silverSpacing = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (analyticalModel == null || string.IsNullOrEmpty(path))
+            {
+                return false;
+            }
+
+            gbXMLSerializer.gbXML gbXML = analyticalModel.TogbXML(silverSpacing, tolerance);
+            if (gbXML == null)
+            {
+                return false;
+            }
+                
+
+            return Core.gbXML.Create.gbXML(gbXML, path);
+        }
+
+    }
+}
